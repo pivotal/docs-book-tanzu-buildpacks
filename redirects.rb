f4a3230cf81733ed351c1319cea5c6e2178bbf2f
Samia Nneji <snneji@vmware.com>
2022-06-24 11:31:58 +0100

    [#182499126] Add redirects

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,9 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+# Redirect from docs.pivotal.io/tanzu-buildpacks to docs.vmware.com Tanzu Buildpacks docs
+
+r301  %r{/tanzu-buildpacks/(.*)}, "https://docs.vmware.com/en/VMware-Tanzu-Buildpacks/services/tanzu-buildpacks/GUID-$1"
+r301  %r{/tanzu-buildpacks/}, "https://docs.vmware.com/en/VMware-Tanzu-Buildpacks/services/tanzu-buildpacks/GUID-index.html"
+r301  %r{/tanzu-buildpacks}, "https://docs.vmware.com/en/VMware-Tanzu-Buildpacks/services/tanzu-buildpacks/GUID-index.html"
